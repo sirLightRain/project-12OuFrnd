d8127288e804de15b12ee737e6f55811ffd5fcc0
okuzmychov <125366168+okuzmychov@users.noreply.github.com>
2023-08-03 18:12:21 +0300

    best-sellers-js

---
+++ src/bestsellers.js
@@ -0,0 +1,44 @@
+const container = document.querySelector('.js-list');
+const PRODUCT_LS_KEY = 'Shopping List';
+
+container.insertAdjacentHTML('beforeend', createMarkup(topBooks));
+container.addEventListener('click', handlerAdd);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ _id, book_image, list_name, author }) => `
+    <li data-id="${_id}" class="js-product">
+        <img src="${book_image}" alt="${list_name}" width="200"/>
+        <h3>${list_name}</h3>
+        <p>${author}</p>
+  </li>`
+    )
+    .join('');
+}
+
+function handlerAdd(evt) {
+  if (!evt.target.classList.contains('js-add')) {
+    return;
+  }
+
+  const products = JSON.parse(localStorage.getItem(PRODUCT_LS_KEY)) ?? [];
+  const product = evt.target.closest('.js-product');
+  const productId = Number(product.dataset.id);
+  const currentProduct = topBooks.find(({ id }) => id === productId);
+  const idx = products.findIndex(({ id }) => id === productId);
+
+  // -1 // true
+  // 0 > ... // false
+
+  // -(0 + 1) //0+1 =>-1 // true
+  // -(-1 + 1)// -0 // false
+  if (idx !== -1) {
+    // !!~idx
+    products[idx].qty += 1;
+  } else {
+    currentProduct.qty = 1;
+    products.push(currentProduct);
+  }
+  localStorage.setItem(PRODUCT_LS_KEY, JSON.stringify(products));
+}
