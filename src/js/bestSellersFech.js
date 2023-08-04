5ba7c1fdd1debed75820f8958bb02f674c123937
Oleksii Stepchuk <34480855+sirLightRain@users.noreply.github.com>
2023-08-04 14:49:43 +0300

    працює погано
    спроба змусити js міняти розмір картинки та форму виводу працює ніяк :)

--- src/js/bestSellersFech.js
+++ src/js/bestSellersFech.js
@@ -1,4 +1,4 @@
-import { createMarkup } from './bestsellers';
+// import { createMarkup } from './bestsellers';
 
 // Функція для отримання топових книг з сервера
 async function fetchTopBooks(url) {
@@ -10,44 +10,95 @@ async function fetchTopBooks(url) {
   return data;
 }
 
+// Функція для створення розмітки одныэъ картки
+function createBookMarkup(arr) {
+  // Визначимо розміри зображення в залежності від розміру екрана
+  let width, height;
+  if (window.innerWidth >= 1440) {
+    width = 180;
+    height = 256;
+  } else if (window.innerWidth >= 768) {
+    width = 218;
+    height = 316;
+  } else {
+    width = 335;
+    height = 485;
+  }
+
+  return arr
+    .map(
+      ({ _id, book_image, list_name, author }) => `
+      <h2>Category: ${list_name}</h2>
+  <li data-id="${_id}" class="js-product">
+      <img src="${book_image}" alt="${list_name}" width='${width}' height='${height}'/>
+      <h3>${list_name}</h3>
+      <p>${author}</p>
+      <div class="js-button-container">
+          <button class="js-see-more">See more</button>
+      </div>
+  </li>
+  `
+    )
+    .join('');
+}
+
 // Функція для створення розмітки для списку книг
-function createBooksListMarkup(books) {
-  return createMarkup(books.slice(0,2));
+function createBooksListMarkup(books, booksInCategory) {
+  return createBookMarkup(books.slice(0, booksInCategory));
 }
 
 // Функція для створення розмітки для категорії
-function createCategoryMarkup(list_name, booksMarkup) {
+function createCategoryMarkup( booksMarkup) {
   return `
-      <h2>Category: ${list_name}</h2>
+
       <ul>${booksMarkup}</ul>
     `;
 }
 
+let booksInCategory;
 // Функція для виведення перших двох категорій з топовими книгами
 async function displayTopBooksByCategory() {
   const url = 'https://books-backend.p.goit.global/books/top-books';
 
+  // Скільки відображати книг вктаегорії залежно від розміру екрану
+  if (window.innerWidth >= 1440) {
+    booksInCategory = 5;
+  } else if (window.innerWidth >= 768) {
+    booksInCategory = 3;
+  } else {
+    booksInCategory = 2;
+  }
+
+  // Скільки виводити категорій на екран залежно від розміру екрану
+  let categoriesToDisplay = 2;
+  if (window.innerWidth >= 768) {
+    categoriesToDisplay = 4;
+  }
+
   try {
     const data = await fetchTopBooks(url);
-    console.log('Перші 2 категорії з топовими книгами: ', data.slice(0, 2));
+    console.log(
+      `Виводимо перші ${categoriesToDisplay} категорії з ${booksInCategory} топовими книгами: `,
+      data.slice(0, booksInCategory)
+    );
 
     const bestSellersContainer = document.querySelector('.js-list-bestsellers');
 
     // Очистимо контейнер перед початком виводу
     bestSellersContainer.innerHTML = '';
 
-    // Виводимо розмітку для перших двох категорій
-    for (const { list_name, books } of data.slice(0, 2)) {
-      const booksMarkup = createBooksListMarkup(books);
-      const categoryMarkup = createCategoryMarkup(list_name, booksMarkup);
+    // Виводимо розмітку для категорій
+    for (const { books } of data.slice(0, categoriesToDisplay)) {
+      const booksMarkup = createBooksListMarkup(books, booksInCategory);
+      const categoryMarkup = createCategoryMarkup(booksMarkup);
       bestSellersContainer.insertAdjacentHTML('beforeend', categoryMarkup);
     }
 
-    // Додаємо обробник події для кнопок "See More"
-    const seeMoreButtons = document.querySelectorAll('.js-see-more');
-    seeMoreButtons.forEach(button => {
-      button.addEventListener('click', handleSeeMore);
-    });
+    //! Додаємо обробник події для кнопок "See More"
+    // const seeMoreButtons = document.querySelectorAll('.js-see-more');
+    // seeMoreButtons.forEach(button => {
+    //   button.addEventListener('click', handleSeeMore);
+    // });
   } catch (error) {
     console.error(error);
   }
