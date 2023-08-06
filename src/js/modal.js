6fef780c080dfffafe702a116ac660131317dd91
anfdrva <angelina0403@ukr.net>
2023-08-06 13:55:58 +0300

    модалка по кліку для кожної книги

--- src/js/modal.js
+++ src/js/modal.js
@@ -19,34 +19,38 @@ const bookList = document.querySelector(".books-list");
 
 serviceBook()
     .then(data => {
-        bookList.insertAdjacentHTML('beforeend', createMarkUp(data))
+        // bookList.insertAdjacentHTML('beforeend', createMarkUp(data))
+        console.log(data)
+        data.forEach(element => {
+            bookList.insertAdjacentHTML('beforeend', createMarkUp(element.books))
+        });
     })
 .catch(err => console.log(err))
 
 
 function createMarkUp(arr) {
-    return arr.map(({ _id, book_image, list_name, author, title }) => `
+    return arr.map(({ _id , book_image, list_name, author, title}) => `
      <li data-id="${_id}" class="js-product">
-      <img src="https://storage.googleapis.com/du-prd/books/images/9781250144058.jpg" alt="${list_name}" class="img-size" loading="lazy"/>
-            <h3 class="book-name-style">${list_name}</h3>
+      <img src="${book_image}" alt="${list_name}" class="img-size" loading="lazy"/>
+            <h3 class="book-name-style">${title}</h3>
             <p class="author-style">${author}</p>
         </li>
         `).join('');
 }
 
 
-function createBookMarkUp() {
-    return `<div class="modal">
+function createBookMarkUp({ _id , book_image, list_name, author, title} = {}) {
+    return `<div class="modal" data-id="${_id}">
     <button class="modal-close">X</button>
-     <img src="https://storage.googleapis.com/du-prd/books/images/9781250144058.jpg" alt="img" class="popup-img" loading="lazy"/>
-     <div class="content>
-<h3 class="title">TITLE</h3>
-            <p class="author">Author</p>
+     <img src="${book_image}" alt="${list_name}" class="popup-img" loading="lazy"/>
+     <div class="content">
+<h3 class="title">${title}</h3>
+            <p class="author">${author}</p>
             <p class="text">Info Info Info Info Info Info Info Info Info Info Info Info</p>
              <ul class="shop-list">
-      <li><a href="#">1</a></li>
-      <li><a href="#">2</a></li>
-      <li><a href="#">3</a></li>
+      <li><a href="#"><img src="./images/amazon.jpg" width="62" height="19" alt="amazon"/></a></li>
+      <li><a href="#"><img src="./images/orange-book.png" width="62" height="19" alt="orange-book"/></a></li>
+      <li><a href="#"><img src="./images/books.png" width="62" height="19" alt="books"/></a></li>
     </ul>
      </div>
             <button class="add-to-cart-btn">ADD TO SHOPPING LIST</button>
@@ -57,16 +61,41 @@ bookList.addEventListener('click', handlerBook)
 
 let currentLightboxInstance = null;
 
-function handlerBook() {
+
+async function handlerBook(evt) {
+  const bookItem = evt.target.closest('.js-product');
+  const bookId = bookItem.dataset.id;
+
+  try {
+    const obj = await findBookItem(bookId);
+    console.log(obj);
 
     closeLightbox();
 
-    const instance = basicLightbox.create(createBookMarkUp());
+    const instance = basicLightbox.create(createBookMarkUp(obj));
     instance.show();
 
     currentLightboxInstance = instance;
 
     document.body.classList.add('disable-scroll');
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+function findBookItem(item) {
+  return serviceBook().then(data => {
+    let currentBook;
+    for (const el of data) {
+      currentBook = el.books.find(({ _id }) => _id === item);
+      if (currentBook) break;
+    }
+    return currentBook || {}; 
+  }).catch(err => {
+    console.log(err);
+    return {}; 
+  });
 }
 
 
@@ -92,4 +121,7 @@ document.addEventListener('keydown', function (event) {
         closeLightbox();
     }
 });
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 
