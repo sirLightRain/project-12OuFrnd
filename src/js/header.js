75e816ed776e0ee9d90915c668f0288d48a5a067
Oleksii Stepchuk <34480855+sirLightRain@users.noreply.github.com>
2023-08-08 05:58:49 +0300

    SupUK працює
    В хедері в js закоментив експорт, бо там іде на нулл

--- src/js/header.js
+++ src/js/header.js
@@ -1,39 +1,41 @@
 (() => {
-    const menuBtnRef = document.querySelector('[data-menu-button]');
-    const mobileMenuRef = document.querySelector('[data-menu]');
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
 
-    menuBtnRef.addEventListener('click', () => {
-        const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-        menuBtnRef.classList.toggle('is-open');
-        menuBtnRef.setAttribute('aria-expanded', !expanded);
-        mobileMenuRef.classList.toggle('is-open');
-    });
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    mobileMenuRef.classList.toggle('is-open');
+  });
 })();
 
 const themeSwitch = document.querySelector('theme-switch');
 const currentСolorSwitch = document.querySelector('theme');
-const elementBody = document.querySelector('body')
-const checkBox = document.querySelector(".theme-checkbox");
+const elementBody = document.querySelector('body');
+const checkBox = document.querySelector('.theme-checkbox');
 
 if (currentСolorSwitch) {
-    elementBody.classList.add(currentСolorSwitch);
-    if (currentСolorSwitch === 'black-theme') {
-        checkBox.ckecked = true;
-    }
+  elementBody.classList.add(currentСolorSwitch);
+  if (currentСolorSwitch === 'black-theme') {
+    checkBox.ckecked = true;
+  }
 }
 
 function switchTheme(e) {
-    if (e.target.ckecked) {
-        elementBody.classList.remove('theme-light');
-        elementBody.classList.add('black-theme');
+  if (e.target.ckecked) {
+    elementBody.classList.remove('theme-light');
+    elementBody.classList.add('black-theme');
 
-        localStorage.setItem('theme', 'black-theme');
-    } else {
-        elementBody.classList.remove('black-theme');
-        elementBody.classList.add('theme-light');
+    localStorage.setItem('theme', 'black-theme');
+  } else {
+    elementBody.classList.remove('black-theme');
+    elementBody.classList.add('theme-light');
 
-        localStorage.setItem('theme', 'theme-light');
-    }
+    localStorage.setItem('theme', 'theme-light');
+  }
 }
-themeSwitch.addEventListener('change,switch,false');
+//! ===================== тимчасовий блок через помилку в консолі 
+// themeSwitch.addEventListener('change,switch,false');
