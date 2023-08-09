9cf08c70023511460282651afcffed7390c8fe5c
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-08-09 22:28:35 +0300

    add Json.parse

--- src/js/conect-shop-list.js
+++ src/js/conect-shop-list.js
@@ -6,19 +6,22 @@ const wrapContainerShoppinglist = (document.querySelector(
 
 function initLocalStorage() {
   // получаем в перемен данные из localStorage
-  let contentLocalStorage = localStorage.getItem('list');
+  let contentLocalStorage = JSON.parse(localStorage.getItem('list'));
+  let lengthContentLocalStorage = contentLocalStorage.length;
+  console.log(lengthContentLocalStorage)
 
   // если длина массива с данными != 0 - ввызываем функцию отрисовки карточек
-  if (contentLocalStorage !== null && contentLocalStorage.length !== 0) {
-    document.querySelector('.wrap-container-shoppinglist').style.display = ' ';
-    document.querySelector('.wrap-empty-desc').style.display = 'none';
-  } else {
-    // если в localStorage что-то есть - вызываем отрисовку пустой корзины
-    document.querySelector('.wrap-container-shoppinglist').style.display =
-      'none';
-    document.querySelector('.wrap-empty-desc').style.display = ' ';
+    if (contentLocalStorage !== null && contentLocalStorage.length !== 0) {
+      document.querySelector('.wrap-container-shoppinglist').style.display = ' ';
+      document.querySelector('.wrap-empty-desc').style.display = 'none';
+    } else {
+      // если в localStorage что-то есть - вызываем отрисовку пустой корзины
+      document.querySelector('.wrap-container-shoppinglist').style.display =
+        'none';
+      document.querySelector('.wrap-empty-desc').style.display = ' ';
+    }
   }
-}
+
 
 // запускаем функцию initLocalStorage при загрузке страницы Shopping List
 document.addEventListener('DOMContentLoaded', initLocalStorage);
