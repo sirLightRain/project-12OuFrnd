df91222cc8445db2fc21f29606f7274681d9c716
Oleksii Stepchuk <34480855+sirLightRain@users.noreply.github.com>
2023-08-10 18:57:18 +0300

    Зняв проблему в консолі
    На стороінці шопінг

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 
-// import { test } from "./js/bestsellers";
-// test("Alex");
-
-// import { displayTopBooksByCategory } from "./js/bestSellersFech";
-import { displayTopBooksByCategory } from "./js/bestsellere/bestSellersOleksii";
-// displayTopBooksByCategory();
-
 //! Ukraine Support
-// import './js/support-ukraine/support.js';
 import '/src/js/support-ukraine/support.js';
-// import './js/support-ukraine/charities.js';
 import '/src/js/support-ukraine/charities.js';
 
 //! Header 
